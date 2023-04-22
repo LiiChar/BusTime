990d77a934828babbd185769ba7caaf33b97a9c8
LiiChar <yura22875@yandex.ru>
2023-04-22 21:19:40 +0500

    text push notification on android

---
+++ index.js
@@ -0,0 +1,32 @@
+const needle = require('needle');
+const express = require('express'); 
+const nodeNotifier = require('node-notifier');
+
+const date = new Date()
+const nowDate = `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}`
+const URL = `https://www.avtovokzal.org/app_php/request_schedule.php?id=1346&date=%27${nowDate}%27&station=nt&platform=web`;
+
+const PORT = 5000
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+app.get('/' , async (req, respons) => {
+    setTimeout(() => {
+        nodeNotifier.notify({
+            title: 'bus starting',
+            message: 'Run',
+        });
+    }, 10000)
+
+    const data = await fetInfo()
+    respons.render('index', {buses: data});
+})
+
+app.listen(PORT, () => console.log(`Server listen on PORT ${PORT}`));
+
+async function fetInfo (){
+    return await needle(URL).then((res) => JSON.parse(res.body).data)
+}
